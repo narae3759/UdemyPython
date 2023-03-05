8b24f948236d179739389b8475f7412ff2ca41a0
나래 <93372888+narae3759@users.noreply.github.com>
2023-03-05 04:36:46 +0000

    README update

--- Project1.py
+++ Project1.py
@@ -1,8 +1,10 @@
 # Project1 : 이름 생성기 만들기
+isquit = 1
+
 print("-" * 70)
 print("안녕하세요! 아이디 추천 프로그램에 오신 것을 환영합니다!")
 print("-" * 70)
-while True:
+while isquit == 1:
   print("다음 3가지의 질문에 대해 답해주세요 :)")
 
   food = input("질문1. 가장 좋아하는 음식이 무엇인가요? > ")
@@ -11,12 +13,14 @@ while True:
 
   print(f"저는 '{food}먹는{color}{animal}'(을)를 추천드릴게요!")
   isquit = int(input("다시 해볼까요?('Y': 1, 'N': 0) > "))
-  if isquit == 1:
-    continue
-  elif isquit == 0:
-    print("-" * 70)
-    print("이용해주셔서 감사합니다 :)")
-    print("-" * 70)
-    break
-  else:
-    isquit = int(input("잘못 입력하셨습니다. 다시 입력해주세요('Y': 1, 'N': 0) > "))
+  while True:
+    if isquit == 1:
+      print("-" * 70)
+      break
+    elif isquit == 0:
+      print("-" * 70)
+      print("이용해주셔서 감사합니다 :)")
+      print("-" * 70)
+      break
+    else:
+      isquit = int(input("잘못 입력하셨습니다. 다시 입력해주세요('Y': 1, 'N': 0) > "))
--- README.md
+++ README.md
@@ -1,8 +1,31 @@
-# **강의명: Python 부트캠프 : 100개의 프로젝트로 Python 개발 완전 정복**
+# **Python 부트캠프 : 100개의 프로젝트로 Python 개발 완전 정복**
 
 # **학습 목표**
 * Python의 주요 내용 정리
 * 100개의 Python과 관련한 다양한 프로젝트 실습
 
 # **진행 상황**
-* Project 1: 이름 생성기 만들기
+<table width="100%">
+  <th>순번</th>
+  <th>프로젝트 명</th>
+  <th>설명</th>
+  <tr>
+      <td>[Project 1](#project-1)</td>
+      <td>아이디 생성기 만들기</td>
+      <td>
+        <li>대화형 코딩 연습</li>
+        <li>3가지의 질문을 통해 아이디를 추천하는 프로그램을 만든다.</li>
+      </td>
+  </tr>
+</table>
+    
+## **Project 1**
+  * 프로젝트명 : 아이디 생성기 만들기
+  * 설명 : 대화형 코딩 연습, 3가지의 질문을 통해 아이디를 추천하는 프로그램을 만든다.
+  <details>
+  <summary>Ouptut</summary>
+  <div markdown="1">
+  <img src="images/project1.png"/>
+  </div>
+  </details>
+    
